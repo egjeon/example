7b0005d41ddd3a65205dbc68acce0ee91cee3563
jeon eun gwang <dmsrhkd132@gmail.com>
2025-12-24 22:45:35 +0900

    PCCP문제 해결

---
+++ src/NinameRegular.java
@@ -0,0 +1,36 @@
+public class NinameRegular {
+    public static String solution(String nickname) {
+        String answer = "";
+        for(int i=0; i<nickname.length(); i++){
+            if(nickname.charAt(i) == 'l'){
+                answer += "I";
+            }
+            else if(nickname.charAt(i) == 'w'){
+                answer += "vv";
+            }
+            else if(nickname.charAt(i) == 'W'){
+                answer += "VV";
+            }
+            else if(nickname.charAt(i) == 'O'){
+                answer += "0";
+            }
+            else{
+                answer += nickname.charAt(i);
+            }
+        }
+
+        if(answer.length() < 3){
+            for(int i=nickname.length()-1; i<3; i++)
+            answer += "o";
+        }
+
+        if(answer.length() > 8){
+            answer = answer.substring(0, 8);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("GO"));
+    }
+}
