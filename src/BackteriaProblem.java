bad3529b90aacc9ee225606223077a636cfa2408
jeon eun gwang <dmsrhkd132@gmail.com>
2026-01-03 22:45:07 +0900

    aa

---
+++ src/BackteriaProblem.java
@@ -0,0 +1,12 @@
+public class BackteriaProblem {
+
+    public static void main(String[] args) {
+        int n = 7;
+        int t = 15;
+        for(int i = 1; i <= t; i++){
+            n *=2;
+        }
+
+        System.out.println(n);
+    }
+}
